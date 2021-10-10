b359d723feb0de432d35268f5aebd0dbe71101b5
kowvincent <91387824+kowvincent@users.noreply.github.com>
2021-10-10 15:10:43 +0800

    Add files via upload

---
+++ yes.js
@@ -0,0 +1,7 @@
+var vg_1 = "barchart.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "map.json";
+vegaEmbed("#choropleth_map", vg_2).then(function(result) {
+}).catch(console.error);
